3d7703f4062fa73a68397d95761c2365147a2c94
Sahil Mahajan <sahilmahajan@Sahils-MacBook-Air.local>
2023-02-12 20:52:08 +0530

    Initial commit

---
+++ src/main/java/com/cognizant/bank/BankApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BankApplication.class, args);
+    }
+}
